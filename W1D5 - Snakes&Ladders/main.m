5e779d5ff330646e7ce23e58ca73d6b3dc329709
brokeandfurious <forumbomb@gmail.com>
2017-11-12 21:46:51 -0500

    Initial Commit

---
+++ W1D5 - Snakes&Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D5 - Snakes&Ladders
+//
+//  Created by Murat Ekrem Kolcalar on 11/12/17.
+//  Copyright © 2017 murtilicious. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
